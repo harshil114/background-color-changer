2e792768251d1574b6430ee49e219719fed7284d
harshil114 <harshilvispute114@gmail.com>
2024-06-05 14:46:15 +0530

    update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="container-bottom">
+          <div className="button-container">
+            <button className='button red'>Red</button>
+            <button className='button green'>Green</button>
+            <button className='button blue'>Blue</button>
+            <button className='button olive'>Olive</button>
+            <button className='button grey'>Grey</button>
+            <button className='button yellow'>Yellow</button>
+            <button className='button pink'>Pink</button>
+            <button className='button purple'>Purple</button>
+            <button className='button lavender'>Lavender</button>
+            <button className='button white'>White</button>
+            <button className='button black'>Black</button>
+          </div>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
